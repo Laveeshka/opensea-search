fefc36c3ca19aeef2dbe9d0f3ea2690bcb6526c6
LaveeshkaMahadea <laveeshka94@gmail.com>
2022-10-06 19:06:51 +1100

    fetch single collection by search without styling

--- src/index.js
+++ src/index.js
@@ -2,13 +2,15 @@
 const options = {method: 'GET', headers: {accept: 'application/json'}};
 let limit = 50;
 let page = 0;
-let collection_slug = 'doodles-official';
+let collection_slug = '';
 let collectionsBaseUrl = `https://api.opensea.io/api/v1/collections?limit=${limit}&offset=${page*limit}`;
 let collectionUrl = `https://api.opensea.io/api/v1/collection/${collection_slug}`;
 const cardsContainer = document.getElementById("cards-container");
 let previousBtn = document.getElementById("previous");
-
-
+const searchBtn = document.getElementById("search");
+const searchInput = document.getElementById("search-name");
+const singleCollectionContainer = document.getElementById("single-collection-container");
+const paginationSection = document.getElementById("pagination");
 
 init(); //hoisting
 
@@ -16,6 +18,7 @@ function init(){
     getNFTCollections();
     nextPage();
     previousPage();
+    searchACollection();
 }
 
 //fetch all
@@ -36,71 +39,27 @@ function getNFTCollections(){
         
 
 function renderCollection(col){
-let card = document.createElement("div");
-card.classList.add("card");
-
-let imageContainer = document.createElement("div");
-imageContainer.classList.add("image-container");
-
-let bannerImg = document.createElement("img");
-bannerImg.classList.add("banner-img");
-let collectionImg = document.createElement("img");
-collectionImg.classList.add("collection-img");
-
-let textContainer = document.createElement("div");
-textContainer.classList.add("text-container");
-
-let leftContainer = document.createElement("div");
-leftContainer.classList.add("left-container");
-let collectionName = document.createElement("div");
-collectionName.classList.add("collection-name");
-let collectionItems = document.createElement("div");
-collectionItems.classList.add("collection-items");
-
-let rightContainer = document.createElement("div");
-rightContainer.classList.add("right-container");
-let totalVolume = document.createElement("div");
-totalVolume.classList.add("total-volume");
-
-let volumeContainer = document.createElement("div");
-volumeContainer.classList.add("volume-container");
-let ethImage = document.createElement("img");
-ethImage.classList.add("eth-logo");
-let volumeValue = document.createElement("div");
-volumeValue.classList.add("volume-value");
-
-    //build image container
-    bannerImg.src = col.banner_image_url === null? "./images/banner_image_default.jpg" : col.banner_image_url;
-    //bannerImg.src = col.banner_image_url;
-    bannerImg.alt = "banner image";
-    collectionImg.src = col.image_url === null? "./images/image_default.jpg" : col.image_url;
-
-    //collectionImg.src = col.image_url;
-    collectionImg.alt = "collection image";
-    imageContainer.append(bannerImg, collectionImg);
-
-    //build left container
-    collectionName.textContent = col.name;
-    collectionItems.textContent = col.stats.count;
-    leftContainer.append(collectionName, collectionItems);
-
-    //build right container
-    totalVolume.textContent = "Total volume";
-    ethImage.src = "./images/eth-logo.svg";
-    ethImage.alt = "Ethereum logo";
-    volumeValue.textContent = col.stats.total_volume;
-    volumeContainer.append(ethImage, volumeValue);
-    rightContainer.append(totalVolume, volumeContainer);
-
-    //build text container
-    textContainer.append(leftContainer, rightContainer);
-
-    //build card container
-    card.append(imageContainer, textContainer);
-
-    //append card to cards container
-    cardsContainer.append(card);
-
+    cardsContainer.innerHTML += `
+        <div class="card">
+                <div class="image-container">
+                    <img class="banner-img" src=${col.banner_image_url === null? "./images/banner_image_default.jpg" : col.banner_image_url} alt="collection banner image">
+                    <img class="collection-img" src=${col.image_url === null? "./images/image_default.jpg" : col.image_url} alt="collection image">
+                </div>
+                <div class="text-container">
+                    <div class="left-container">
+                        <div class="collection-name">${col.name}</div>
+                        <div class="collection-items">${col.stats.count} items</div>
+                    </div>
+                    <div class="right-container">
+                        <div class="total-volume">Total volume</div>
+                        <div class="volume-container">
+                            <img class="eth-logo" src="./images/eth-logo.svg" alt="Ethereum logo">
+                            <div class="volume-value">${col.stats.total_volume}</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    `
 }
 
 function nextPage(){
@@ -133,6 +92,62 @@ function previousPage(){
         }
         console.log(`page after previous: ${page}`);
     })
+}
+
+function searchACollection(){
+    searchBtn.addEventListener("click", searchCollectionHander);
+}
+
+function searchCollectionHander(event){
+    //hide cards and pagination
+    cardsContainer.style.display = 'none';
+    paginationSection.style.display = 'none';
+
+    collection_slug = searchInput.value;
+
+    fetch(`https://api.opensea.io/api/v1/collection/${collection_slug}`, options)
+        .then(res => res.json())
+        .then(colObj => renderSingleCollection(colObj.collection))
+        .catch(err => console.log(err));
+}
+
+function renderSingleCollection(col){
+    //console.log(col);
+    const {banner_image_url, name, image_url, description, external_url, stats: {floor_price, count, total_volume}} = col;
+    console.log(banner_image_url, name, image_url, description, external_url, floor_price, count, total_volume);
+
+    //build html elements for collection details
+    singleCollectionContainer.innerHTML = `        
+    <div class="image-container">
+    <img class="banner-img" src=${banner_image_url === null? "./images/banner_image_default.jpg" : banner_image_url} alt="collection banner image">
+    <img class="collection-img" src=${image_url === null? "./images/image_default.jpg" : image_url} alt="collection image">
+</div>
+<div class="text-container wrapper-container">
+    <h2 class="collection-name">${name}</h2>
+    <a href=${external_url} target="_blank">Go to collection</a>
+    <p class="description">${description}</p>
+    <div class="stats-container">
+        <div class="stats">
+            <div class="collection-items">Items</div>
+            <div class="collection-count">${count}</div>
+        </div>
+        <div class="stats">
+            <div class="total-volume">Total volume</div>
+            <div class="volume-container">
+                <img class="eth-logo" src="./images/eth-logo.svg" alt="Ethereum logo">
+                <div class="volume-value">${total_volume}</div>
+            </div>
+        </div>
+        <div class="stats">
+            <div class="floor-price">Floor price</div>
+            <div class="floor-container">
+                <img class="eth-logo" src="./images/eth-logo.svg" alt="Ethereum logo">
+                <div class="floor-value">${floor_price}</div>
+            </div>
+        </div>
+    </div>
+</div>
+`
 }
 
 //park sorting for now
